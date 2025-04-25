09d972aac84573a7316fd757a058e2887c8b34e6
Bruno Lacerda <bruno.lacerda@veolink.com.br>
2025-04-25 10:21:33 -0300

    add spring project

---
+++ src/main/java/br/pw3/sistema/veiculos/PRW3_Projeto_Spring/Prw3ProjetoSpringApplication.java
@@ -0,0 +1,13 @@
+package br.pw3.sistema.veiculos.PRW3_Projeto_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prw3ProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prw3ProjetoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/pw3/sistema/veiculos/PRW3_Projeto_Spring/Prw3ProjetoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.pw3.sistema.veiculos.PRW3_Projeto_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prw3ProjetoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
